5ee72aaa2f41230d6db6a59309aed4a549074569
kev <vimagick@gmail.com>
2016-09-22 00:40:24 +0800

    keep only the 1st url

--- shoutcast.sh
+++ shoutcast.sh
@@ -5,7 +5,7 @@
 
 curl -s -X POST -H 'Content-Length: 0' http://www.shoutcast.com/Home/Top |
   jq '.[].ID' |
-    parallel --eta -k curl -s 'http://yp.shoutcast.com/sbin/tunein-station.m3u?id={}' |
+    parallel --eta -k 'curl -s "http://yp.shoutcast.com/sbin/tunein-station.m3u?id={}" | head -3' |
       sed '1!s@#EXTM3U@@' |
         cat -s |
           gzip > shoutcast_$(date +%F).m3u.gz
